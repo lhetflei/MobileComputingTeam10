42c8c165edc4fd2ac4edba4a1341eaf0c1c9407a
Loris Hetfleisch <loris.hetfleisch@smail.th-koeln.de>
2022-01-17 14:01:13 +0100

    firebase

--- app/src/main/java/com/example/lendify/MainActivity.kt
+++ app/src/main/java/com/example/lendify/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.lendify.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
 
