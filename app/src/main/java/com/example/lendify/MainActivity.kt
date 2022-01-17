df1e7547c691bfe4df898059588faa1bd298b340
Loris Hetfleisch <loris.hetfleisch@smail.th-koeln.de>
2022-01-17 12:16:58 +0100

    Registration Activity

--- app/src/main/java/com/example/lendify/MainActivity.kt
+++ app/src/main/java/com/example/lendify/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.lendify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.lendify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+    val myButton: Button = findViewById(R.id.ButtonLogin)
+
 }
