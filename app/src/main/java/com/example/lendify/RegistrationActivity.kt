1e911ab589e6e6c390e8206e93e3432ce88871be
Loris Hetfleisch <loris.hetfleisch@smail.th-koeln.de>
2022-01-19 16:11:39 +0100

    Firebase Registration

--- app/src/main/java/com/example/lendify/RegistrationActivity.kt
+++ app/src/main/java/com/example/lendify/RegistrationActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lendify.databinding.ActivityMainBinding
 import com.example.lendify.databinding.ActivityRegistrationBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegistrationActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityRegistrationBinding
-
+    val ref = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
@@ -20,10 +23,18 @@ class RegistrationActivity : AppCompatActivity() {
 
     }
     fun register(){
-        if(binding.EditTextName.text.isNotEmpty()&&binding.EditTextEmail.text.isNotEmpty()&&binding.EditTextPassword.text.isNotEmpty())
-        binding.TextView1.text="Erfolg"
-        else
-            binding.TextView1.text="Fehler"
+        binding.ButtonRegister.setOnClickListener{
+            val email = binding.EditTextEmail.text.toString().trim()
+            val password = binding.EditTextPassword.text.toString().trim()
+
+            ref.createUserWithEmailAndPassword(
+                email, password
+            )
+        }
     }
 
 }
+/*if(binding.EditTextName.text.isNotEmpty()&&binding.EditTextEmail.text.isNotEmpty()&&binding.EditTextPassword.text.isNotEmpty())
+        binding.TextView1.text="Erfolg"
+        else
+            binding.TextView1.text="Fehler"*/
